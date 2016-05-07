209771138df2477f14902083e1c59bd3c629b099
Anko Anchev <anko.anchev@gmail.com>
2016-05-07 16:36:40 +0100

    Movement scripts

---
+++ Assets/Scripts/Falling.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Falling : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        doMove();
+    }
+
+    void Update()
+    {
+        doMove();
+    }
+
+
+    void doMove()
+    {
+        Vector3 movement = new Vector3(rb.velocity.x, speed, rb.velocity.z);
+        rb.velocity = movement;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update ()
+    {
+        if (speed == 0)
+            return;
+        
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0f, moveVertical);
+        movement *= speed;
+
+        movement.y = rb.velocity.y; //don't change the Y velocity
+
+		rb.velocity = movement;
+    }
+}
