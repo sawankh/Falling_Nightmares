caa843ad409d6d45519bb9678699d2df259e9b43
Anko Anchev <anko.anchev@gmail.com>
2016-05-08 15:13:56 +0100

    Tunnel Generation and cleanup

---
+++ Assets/Scripts/EndlessTunnel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class EndlessTunnel : MonoBehaviour {
+
+    public List<GameObject> modules;
+    private GameObject lastModule;
+
+    void Awake()
+    {
+        lastModule = transform.GetChild(transform.childCount - 1).gameObject;
+    }
+
+
+    void AppendModule()
+    {
+        GameObject module = modules[Random.Range(0, modules.Count)];
+        AppendModule(module);
+    }
+
+    void AppendModule(GameObject module)
+    {
+        GameObject m = Instantiate(module);
+        m.transform.parent = this.gameObject.transform;
+        m.transform.position = lastModule.transform.position - new Vector3(0, 10, 0); //#! Change '10' to a correct value !#
+
+        lastModule = m;
+    }
+}
---
+++ Assets/Scripts/GarbageCollector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GarbageCollector : MonoBehaviour {
+
+    public GameObject tunnelGenerator;
+
+    void Awake()
+    {
+        if (tunnelGenerator == null)
+            tunnelGenerator = GameObject.Find("Tunnel");
+    }
+
+
+    private bool busy = false;
+
+    void Update()
+    {
+        busy = false;
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (busy) return;
+
+        GameObject o = other.gameObject;
+        while (o != null && o.tag != "TunnelModule")
+            o = o.transform.parent.gameObject;
+
+        if (o == null)
+            return;
+
+        Destroy(o);
+        tunnelGenerator.SendMessage("AppendModule");
+        busy = true;
+    }
+}
