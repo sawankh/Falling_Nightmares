713a5378154097b7f19ba964109a86bfb0b7ad31
Anko Anchev <anko.anchev@gmail.com>
2016-05-10 21:18:52 +0100

    Added Score script and GradualEffects script

---
+++ Assets/Scripts/GradualEffects.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GradualEffects : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(GainScore());
+        StartCoroutine(GradualSpeedUp());
+    }
+
+    IEnumerator GainScore()
+    {
+        Score score = Score.instance;
+
+        while (true)
+        {
+            score.score += 1;
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+
+    IEnumerator GradualSpeedUp()
+    {
+        float step = 0.01f;
+
+        while (Time.timeScale < 100 - step)
+        {
+            Time.timeScale += step;
+            yield return new WaitForSeconds(1f / Time.timeScale);
+        }
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour
+{
+    public static Score instance { get; private set; }
+
+
+    public int score
+    {
+        get
+        {
+            return _score;
+        }
+        set
+        {
+            _score = value;
+            UpdateLabel();
+        }
+    }
+    private int _score;
+
+    public Text label;
+    [Tooltip("Gets the value on the actual Text component if this is empty.")]
+    public string labelText;
+
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+        {
+            Destroy(instance);
+            instance = this;
+        }
+
+
+        if (label == null)
+            label = GetComponent<Text>();
+
+        if (label == null)
+            Debug.LogError("No Text component is referenced to use as label!");
+
+        if (labelText == "")
+            labelText = label.text;
+    }
+
+    void UpdateLabel()
+    {
+        label.text = labelText + score;
+    }
+}
