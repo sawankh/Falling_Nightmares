6f097c0fe1283538de6b8bccba62c3157a67bc9f
Anko Anchev <anko.anchev@gmail.com>
2016-05-10 21:25:02 +0100

    Added Death triggers and DeathScreen

---
+++ Assets/Scripts/DeathByContact.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class DeathByContact : MonoBehaviour {
+
+    public DeathScreen deathScreen;
+
+    private bool dead = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Die();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        GameObject o = other.collider.gameObject;
+        int obstacleLayer = LayerMask.NameToLayer("Obstacle");
+
+        while (o.layer != obstacleLayer)
+        {
+            if (o.transform.parent == null)
+                return;
+
+            o = o.transform.parent.gameObject;
+        }
+
+        Die();
+    }
+
+    void Die()
+    {
+        if (dead) return;
+
+        deathScreen.Dead();
+
+        dead = true;
+    }
+}
---
+++ Assets/Scripts/DeathScreen.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DeathScreen : MonoBehaviour {
+
+    public Text label;
+    public string labelText;
+
+    void Awake()
+    {
+        gameObject.SetActive(false);
+
+        if (label != null && labelText == "")
+            labelText = label.text;
+    }
+
+    public void Dead()
+    {
+        gameObject.SetActive(true);
+        Time.timeScale = 0;
+
+        if (label != null && label.text != "")
+        {
+            int score = Score.instance.score;
+            label.text = string.Format(labelText, score);
+        }
+    }
+
+    public void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+        Time.timeScale = 1;
+    }
+}
