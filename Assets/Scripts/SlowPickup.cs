cf27e80b8cdfe961c52d338efee79cdd0b273ba1
Anko Anchev <anko.anchev@gmail.com>
2016-05-11 01:31:38 +0100

    PowerUp "Slow"

---
+++ Assets/Scripts/SlowPickup.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowPickup : MonoBehaviour {
+
+    public float slowAmount = 2f;
+
+    private bool busy = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!IsPlayer(other.gameObject) || busy)
+            return;
+
+        busy = true;
+        Destroy(transform.parent.gameObject); //#! gradual
+        Time.timeScale = Mathf.Max(1, Time.timeScale - slowAmount);
+    }
+
+    bool IsPlayer(GameObject o)
+    {
+        while (o.tag != "Player")
+        {
+            if (o.transform.parent == null)
+                return false;
+
+            o = o.transform.parent.gameObject;
+        }
+        return true;
+    }
+}
