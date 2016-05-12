d950f81f08e25b111b127a1d7902c2e32969eb8e
Anko Anchev <anko.anchev@gmail.com>
2016-05-12 12:41:28 +0100

    Added arrows stock image

---
+++ Assets/TimeoutRemove.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeoutRemove : MonoBehaviour {
+
+	void Start () {
+        Invoke("Hide", 2f);
+	}
+
+    void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
